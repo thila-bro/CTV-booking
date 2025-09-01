863f42ff319d0d7539e0905b1db5af3952270fee
thila-bro <thilanmaduranga37@yahoo.com>
2025-09-01 10:37:14 +0800

    folder structure updates

--- app/admin/(with-layout)/(overview)/page.tsx
+++ app/admin/(with-layout)/(overview)/page.tsx
@@ -1,13 +1,13 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Card } from '@/app/ui/dashboard/admin/cards';
+import RevenueChart from '@/app/ui/dashboard/admin/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/admin/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 
 
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardSkeleton } from '@/app/ui/skeletons';
-import CardWrapper from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/admin/cards';
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
--- app/admin/(without-layout)/login/page.tsx
+++ app/admin/(without-layout)/login/page.tsx
@@ -6,7 +6,7 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from '../ui/button';
+import { Button } from '@/app/ui/button';
 import Form from 'next/form';
 import { login } from '@/app/lib/admin';
 import { useActionState } from 'react';
